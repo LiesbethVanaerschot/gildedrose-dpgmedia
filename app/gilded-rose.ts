8ddea3cd07f41b283c00c34a83548ffd55537ee9
Liesbeth Vanaerschot <liesbeth.vanaerschot@viavictor.com>
2025-01-20 17:48:07 +0100

    first commit typescript

---
+++ app/gilded-rose.ts
@@ -0,0 +1,73 @@
+export class Item {
+  name: string;
+  sellIn: number;
+  quality: number;
+
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+export class GildedRose {
+  items: Array<Item>;
+
+  constructor(items = [] as Array<Item>) {
+    this.items = items;
+  }
+
+  updateQuality() {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].quality > 0) {
+          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.items[i].quality - 1
+          }
+        }
+      } else {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1
+          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            // Liesbeth TODO: can be written in one if statement
+            if (this.items[i].sellIn < 11) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+            if (this.items[i].sellIn < 6) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+          }
+        }
+      }
+      //Liesbeth TODO: should the sellIn date not be updated first? Or together with Q as the normal start.
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+      }
+      if (this.items[i].sellIn < 0) {
+        if (this.items[i].name != 'Aged Brie') {
+          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.items[i].quality > 0) {
+              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].quality = this.items[i].quality - 1
+              }
+            }
+          } else {
+            this.items[i].quality = this.items[i].quality - this.items[i].quality
+          }
+        } else {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1
+          }
+        }
+      }
+    }
+    //TODO: minder geneste if statements
+    //TODO: add conjured item SI -1 en Q -2
+
+    return this.items;
+  }
+}
